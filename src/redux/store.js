78e436ef5709aba513373dfd88a26d148c21bd2c
AlexanderDmitriev <alex9110077@gmail.com>
2022-05-11 22:56:29 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice  } from '@reduxjs/toolkit';
+
+const initialState= JSON.parse(window.localStorage.getItem('contacts')) ?? [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+const phoneBookSlice = createSlice(
+  {name:"contacts", initialState, reducers:{
+    saveContact(state,action){},
+    deleteContact(state,action){},
+    filterContacts(state,action){},
+  }}
+);
 
 export const store = configureStore({
-  reducer: {},
-})
+  reducer: {contacts:phoneBookSlice.reducer},
+});
+
+export const {saveContact,deleteContact,filterContacts} = phoneBookSlice.actions;
